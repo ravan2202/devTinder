f10dc0fbe0051c136505734dc7aa271ae7a19ca9
ravan2202 <iamvikivivek2202@gmail.com>
2024-09-27 12:12:07 +0530

    dynamic-signup

--- src/app.js
+++ src/app.js
@@ -3,13 +3,11 @@ const connectDB = require("./config/database");
 const User  = require("./models/user");
 const app = express();
 
+app.use(express.json())
+
 app.post("/signup", async (req,res) =>{
-    const user = new User({
-       firstName: "Virat",
-       lastName: "Kohli",
-       emailID: "virat.kohli@gmail.com",
-       password: "Virat@123",
-    });
+    
+    const user = new User(req.body);
     try{
         await user.save();
         res.send("User Added Succesfully");
